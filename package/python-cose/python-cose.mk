d6ebf739d922fe3a500b3277c8da9de94a7e4cac
Adam Kulesza <adam.kulesza@arm.com>
2022-07-20 18:55:09 +0200

    Adds python-cose package

---
+++ package/python-cose/python-cose.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# iat-verifier
+#
+################################################################################
+
+PYTHON_COSE_VERSION = 651dacb8cf7b17a8f47e90650f15c4e9b6ce6240 
+PYTHON_COSE_SOURCE = python-cose-$(IAT_VERIFIER_VERSION).tar.gz
+PYTHON_COSE_SITE = git@github.com:adam2809/pycose.git
+PYTHON_COSE_SITE_METHOD = git
+PYTHON_COSE_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
