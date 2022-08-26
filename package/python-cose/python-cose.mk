af52614d0ec9ca7a7524fc2374b7dba227c17fd5
Adam Kulesza <adam.kulesza@arm.com>
2022-08-26 12:24:56 +0200

    Changes package download method to https
    Signed-off-by: Adam Kulesza <adam.kulesza@arm.com>
    Change-Id: I0bff8ef7a0438d6986057cb61b3443bf9e705f29

--- package/python-cose/python-cose.mk
+++ package/python-cose/python-cose.mk
@@ -6,7 +6,7 @@
 
 PYTHON_COSE_VERSION = 651dacb8cf7b17a8f47e90650f15c4e9b6ce6240 
 PYTHON_COSE_SOURCE = python-cose-$(IAT_VERIFIER_VERSION).tar.gz
-PYTHON_COSE_SITE = git@github.com:adam2809/pycose.git
+PYTHON_COSE_SITE = https://github.com/adam2809/pycose.git
 PYTHON_COSE_SITE_METHOD = git
 PYTHON_COSE_SETUP_TYPE = setuptools
 
